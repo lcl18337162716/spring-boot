1617edcdbc34b63b47ab221cb4d3e3946bfd0c58
cliu <cliu@metchange.com>
2017-11-08 14:28:40 +0800

    初始化

--- manager/src/main/java/com/lcl/manager/login/controller/LoginController.java
+++ manager/src/main/java/com/lcl/manager/login/controller/LoginController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping(value = "login")
-public class LoginController {
+public class LoginController  {
 
     @Autowired
     private JwtTokenUtil jwtTokenUtil;
